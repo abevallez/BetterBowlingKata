6e9360b41f5a899447df984827ca9cc90ef7aaa3
Abraham Vallez <abevallez@gmail.com>
2017-03-23 21:12:58 +0100

    stringWithOneFrameAndPinDownsReturnGameWithOneFrameAndPinByRollDefined

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -17,7 +17,7 @@ public class GameTest {
     }
 
     @Test
-    public void stringWithOneRollAndPinDownsReturnGameWithOneFrameAndPinByRollDefined()
+    public void stringWithOneFrameAndPinDownsReturnGameWithOneFrameAndPinByRollDefined()
     {
         String annotation = "01";
         int [][] expectedFrames = new int [10][2];
@@ -25,5 +25,18 @@ public class GameTest {
         expectedFrames[0][1] = 1;
         Game game = new Game(annotation);
         assertArrayEquals("game must have 1 frame with 2 roll and 1 pin in last roll", expectedFrames, game.frames);
+    }
+
+    @Test
+    public void stringWithTwoFramesGameWithTwoFrameAndPinByRollDefined()
+    {
+        String annotation = "0102";
+        int [][] expectedFrames = new int [10][2];
+        expectedFrames[0][0] = 0;
+        expectedFrames[0][1] = 1;
+        expectedFrames[1][0] = 0;
+        expectedFrames[1][1] = 2;
+        Game game = new Game(annotation);
+        assertArrayEquals("game must have 2 frames and pin downs defined", expectedFrames, game.frames);
     }
 }
