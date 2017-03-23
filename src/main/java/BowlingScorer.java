79edb1fe70ce3829360ca98400e6f7fb4f70988b
Abraham Vallez <abevallez@gmail.com>
2017-03-23 20:02:58 +0100

    only1PinDownScore1

--- src/main/java/BowlingScorer.java
+++ src/main/java/BowlingScorer.java
@@ -1,6 +1,10 @@
 public class BowlingScorer {
 
     public int calculateScore(String gameAnnotation) {
+        if (gameAnnotation.contains("1")) {
+            return 1;
+        }
+
         return 0;
     }
 }
--- src/test/java/BowlingScorerTest.java
+++ src/test/java/BowlingScorerTest.java
@@ -8,6 +8,14 @@ public class BowlingScorerTest {
     public void gutterGameScore0() {
         BowlingScorer bowlingScorer = new BowlingScorer();
         int totalScore = bowlingScorer.calculateScore("00000000000000000000");
-        assertEquals("a gutter game must score 0", 0, totalScore);
+        assertEquals("a gutter game must score 0 points", 0, totalScore);
+    }
+
+    @Test
+    public void only1PinDownScore1()
+    {
+        BowlingScorer bowlingScorer = new BowlingScorer();
+        int totalScore = bowlingScorer.calculateScore("10000000000000000000");
+        assertEquals("only 1 pin down must return 1 point", 1, totalScore);
     }
 }
