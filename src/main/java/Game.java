0e211957db8e3f7c37916914962f2388ccc4c1ee
Abraham Vallez <abevallez@gmail.com>
2017-05-07 12:29:50 +0200

    refactor in Game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -9,6 +9,8 @@ public class Game {
     static final char STRIKE_SIGNAL = 'X';
     static final int STRIKE_VALUE = 10;
     static final int SPARE_VALUE = 10;
+    static final int NUMBER_OF_FRAMES_IN_GAME = 10;
+    static final int NO_PINS = 0;
 
     /**
      * Frames annotation in a Game
@@ -29,25 +31,41 @@ public class Game {
     }
 
     /**
+     *
+     * @return
+     */
+    public List<Frame> getFrames() {
+        return frames;
+    }
+
+    /**
      * Set Frames from annotation
      *
      * @param annotation
      */
     private void setFrames(String annotation) {
+        String lastFrameAnnotation = processAllGameUntilLastFrame(annotation);
+        processLastFrame(lastFrameAnnotation);
+    }
+
+    /**
+     *
+     * @param annotation
+     * @return
+     */
+    private String processAllGameUntilLastFrame(String annotation) {
         char firstRoll;
         char secondRoll;
-        char firstExtraRoll;
-        char secondExtraRoll;
-
+        String lastFrameAnnotation;
         int i;
         int j;
 
-        for (i = 0, j = 0; i<=8; i++) {
+        for (i = 0, j = 0; i < NUMBER_OF_FRAMES_IN_GAME - 1; i++) {
             firstRoll = annotation.charAt(j);
             secondRoll = annotation.charAt(j+1);
 
             if (STRIKE_SIGNAL == firstRoll) {
-                this.frames.add(i, new Frame(signalToPoints(STRIKE_SIGNAL), 0));
+                this.frames.add(i, new Frame(signalToPoints(STRIKE_SIGNAL), NO_PINS));
                 j++;
             } else {
                 this.frames.add(i, new Frame(signalToPoints(firstRoll), signalToPoints(secondRoll)));
@@ -55,35 +73,78 @@ public class Game {
             }
         }
 
-        firstRoll = annotation.charAt(j);
-        secondRoll = annotation.charAt(j + 1);
-
-        if (STRIKE_SIGNAL == firstRoll) {
-            firstExtraRoll = annotation.charAt(j + 2);
-            secondExtraRoll = annotation.charAt(j + 3);
-            this.frames.add(i, new LastFrame(STRIKE_VALUE, 0, signalToPoints(firstExtraRoll), signalToPoints(secondExtraRoll)));
-        } else {
-            LastFrame lastFrame = new LastFrame(signalToPoints(firstRoll), signalToPoints(secondRoll));
-            if (SPARE_VALUE == lastFrame.getScore()) {
-                firstExtraRoll = annotation.charAt(j + 2);
-                lastFrame = new LastFrame(signalToPoints(firstRoll), signalToPoints(secondRoll), signalToPoints(firstExtraRoll));
-            }
-            this.frames.add(i, lastFrame);
-        }
+        lastFrameAnnotation = annotation.substring(j);
+        return lastFrameAnnotation;
     }
 
+    /**
+     *
+     * @param lastFrameAnnotation
+     */
+    private void processLastFrame(String lastFrameAnnotation) {
+        char firstRoll;
+        char secondRoll;
+        char lastIndexFrame = 9;
+
+        firstRoll = lastFrameAnnotation.charAt(0);
+        secondRoll = lastFrameAnnotation.charAt(1);
+        LastFrame lastFrame;
+
+        if (STRIKE_SIGNAL == firstRoll) {
+            lastFrame = getStrikeLastFrame(lastFrameAnnotation);
+        } else {
+            lastFrame = getSpareLastFrame(lastFrameAnnotation, firstRoll, secondRoll);
+        }
+        this.frames.add(lastIndexFrame, lastFrame);
+
+    }
+
+    /**
+     *
+     * @param lastFrameAnnotation
+     * @param firstRoll
+     * @param secondRoll
+     * @return
+     */
+    private LastFrame getSpareLastFrame(String lastFrameAnnotation, char firstRoll, char secondRoll) {
+        LastFrame lastFrame;
+        char firstExtraRoll;
+        lastFrame = new LastFrame(signalToPoints(firstRoll), signalToPoints(secondRoll));
+        if (SPARE_VALUE == lastFrame.getScore()) {
+            firstExtraRoll = lastFrameAnnotation.charAt(2);
+            lastFrame = new LastFrame(signalToPoints(firstRoll), signalToPoints(secondRoll), signalToPoints(firstExtraRoll));
+        }
+        return lastFrame;
+    }
+
+    /**
+     *
+     * @param lastFrameAnnotation
+     * @return
+     */
+    private LastFrame getStrikeLastFrame(String lastFrameAnnotation) {
+        char firstExtraRoll;
+        char secondExtraRoll;
+        LastFrame lastFrame;
+        firstExtraRoll = lastFrameAnnotation.charAt(2);
+        secondExtraRoll = lastFrameAnnotation.charAt(3);
+        lastFrame = new LastFrame(STRIKE_VALUE, NO_PINS, signalToPoints(firstExtraRoll), signalToPoints(secondExtraRoll));
+        return lastFrame;
+    }
+
+    /**
+     *
+     * @param signal
+     * @return
+     */
     private int signalToPoints(char signal) {
         switch (signal) {
             case 'X':
                 return STRIKE_VALUE;
             case '-':
-                return 0;
+                return NO_PINS;
             default:
                 return Character.getNumericValue(signal);
         }
-    }
-
-    public List<Frame> getFrames() {
-        return frames;
     }
 }
